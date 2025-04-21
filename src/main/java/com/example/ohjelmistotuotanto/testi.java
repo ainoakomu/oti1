e99240b0059fee2f899778e666512b689fe0fd5d
Aino Komulainen <ainokaroliinakomulainen@gmail.com>
2025-04-21 13:43:11 +0300

    alkuikkuna raakaversio,tervetuloikkuna metodi aloitus

--- src/main/java/com/example/ohjelmistotuotanto/testi.java
+++
@@ -1,7 +0,0 @@
-package com.example.ohjelmistotuotanto;
-
-public class testi {
-    public static void main(String[] args){
-        System.out.println("Olen tehnyt muutoksia ja ne näkyy");
-    }
-}
