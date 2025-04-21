514d3b865e4a84e00a0a33fcdde0eea79c22c55f
Aino Komulainen <ainokaroliinakomulainen@gmail.com>
2025-04-21 13:43:11 +0300

    alkuikkuna raakaversio,tervetuloikkuna metodi aloitus

--- src/main/java/com/example/ohjelmistotuotanto/Testiikkuna.java
+++ src/main/java/com/example/ohjelmistotuotanto/Testiikkuna.java
@@ -1,14 +1,14 @@
 package com.example.ohjelmistotuotanto;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -21,32 +21,39 @@ public class Testiikkuna extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        //saadaan esille ikkuna
+        primaryStage.setScene(luoKirjatumisIkkuna());
+        primaryStage.show();
+
+
+    }
+
+    public static Scene luoKirjatumisIkkuna(){
         BorderPane rootPaneeli=new BorderPane();
         Scene ruutu=new Scene(rootPaneeli,400,400);
 
         //hbox for logging in
         VBox kirjautumisLaatikko=new VBox();
         kirjautumisLaatikko.setSpacing(10);
+        kirjautumisLaatikko.setPadding(new Insets(5,50,5,50));
         kirjautumisLaatikko.setAlignment(Pos.CENTER);
         Text nimi=new Text("VillaBOOK");
         Text login=new Text("Kirjaudu sisään");
-        TextField kayttajatunnus=new TextField("Käyttäjätunnus");
-        kayttajatunnus.setPrefWidth(50);
-        kayttajatunnus.setPrefHeight(20);
+        TextField kayttajatunnus=new TextField();
+        kayttajatunnus.setPromptText("Käyttäjätunnus");
+        kayttajatunnus.setPrefWidth(10);
         PasswordField salasana=new PasswordField();
-        salasana.setPrefWidth(50);
-        salasana.setPrefHeight(20);
+        salasana.setPromptText("Salasana");
+
         Button kirjautumisButton=new Button("Kirjaudu sisään");
         kirjautumisLaatikko.getChildren().addAll(nimi,login,kayttajatunnus,salasana,kirjautumisButton);
         rootPaneeli.setCenter(kirjautumisLaatikko);
+        return ruutu;
+    }
 
-
-
-
-
-        primaryStage.setScene(ruutu);
-        primaryStage.show();
-
-
+    public static Scene luoTervetuloIkkuna(){
+        GridPane rootPaneeli=new GridPane();
+        Scene ruutu2 =new Scene(rootPaneeli,400,400);
+        return ruutu2;
     }
 }
