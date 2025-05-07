d975b899878713b9e5be9c6abdbfa4813221e90a
evita romppanen <evita.romppanen@gmail.com>
2025-05-07 11:25:52 +0300

    - Resources kansioon lisätty kaksi kuvaa - Luokka taustakuvat, luo taustakuvia. TÄnne tehdään jatkossa muutkin taustakuva metodit kutsuttavaksi. - luoKirjautumisIkkunaan lisätty otsikko kuva, ja kutsutaan taustakuvaa.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.ohjelmistotuotanto {
     requires javafx.fxml;
     requires org.apache.pdfbox;
     requires java.sql;
+    requires java.desktop;
 
 
     exports com.example.ohjelmistotuotanto;
